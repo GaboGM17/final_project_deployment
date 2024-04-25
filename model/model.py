084d4e91853d0f2436c61570e7f8513f88b2708c
Gabriel Guzman <gguzman@boss.tech>
2024-04-25 00:50:28 -0600

    Split the data

--- model/model.py
+++ model/model.py
@@ -8,4 +8,10 @@ from sklearn.pipeline import Pipeline
 from sklearn.metrics import mean_absolute_error, r2_score
 import joblib
 from sklearn.model_selection import cross_val_score
-#%%
+
+df = pd.read_csv('../datasets/housing_df.csv')
+y = df['MEDV']
+X = df.drop(['MEDV'], axis=1)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
